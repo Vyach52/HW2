e84d2563fedcbce702aa999423fab4023576c344
Vyach52 <Sslavih81@gmail.com>
2022-12-06 19:34:20 +0300

    Домашка на переменные первая

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main {
+    public static void main(String[] args)
+    {
+         var dog = 8.0;
+         System.out.println(dog);
+         var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat +4;
+        paper = paper +4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+    var friend = 19;
+        System.out.println(friend);
+    friend = friend * 2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        System.out.println(
+
+        );
+System.out.println("Задача о боксерах. ");
+        System.out.println(
+
+        );
+var boxerfirst = 78.2;
+        System.out.println("Боксер первый " + boxerfirst + " кг ");
+        var boxersecond = 82.7;
+        System.out.println("Боксер второй " + boxersecond + " кг ");
+        var massa = boxerfirst + boxersecond;
+        System.out.println("Общая масса боксеров " + massa + " кг ");
+        var delta = boxersecond - boxerfirst;
+        System.out.println(" Разница масс " + delta + " КГ ");
+        var delta2 = boxersecond % boxerfirst;
+        System.out.println("Второй боксер больше первого " + delta2 + " на кг");
+        System.out.println(
+
+        );
+        System.out.println("Задача про количество работников.");
+        System.out.println(
+
+        );
+        var time = 640;
+        var timeEmployee = 8;
+        var employee = time / timeEmployee;
+        System.out.println("Всего работников компании - " + employee + " человек");
+        employee = employee + 94;
+        timeEmployee = employee * timeEmployee;
+        System.out.println("Если в компании работает " + employee + " человек, то " + timeEmployee + " часов работы может быть поделено между сотрудниками");
+    }}
